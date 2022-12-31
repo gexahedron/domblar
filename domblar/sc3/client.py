603eb31bf31aeeeb6955e33d276c910627a2393f
gexahedron <ulyanick@gmail.com>
2022-12-31 13:32:34 +0100

    Created __init__.py files, moved files to domblar subdirectory

--- domblar/sc3/client.py
+++ domblar/sc3/client.py
@@ -1,5 +1,5 @@
 import pyOSC3
-from music_math import freq2midi
+from ..music_math import freq2midi
 
 
 def send_msg(client, address, data, timetag=0):
@@ -10,13 +10,13 @@ def send_msg(client, address, data, timetag=0):
     client.send(bundle)
 
 
-def send_note(synth_idx, freq=None, dur=0.25, timetag=0, channel=0):
+def send_note(client, synth_idx, freq=None, dur=0.25, timetag=0, channel=0):
     if freq is None:
         return
-    # todo:
+    # TODO:
     # if freq is not None:
     #     data += ['freq', freq]
     data = [synth_idx]
     note, bend = freq2midi(freq)
     data.extend([note, bend, dur, channel])
-    send_msg('/tracker', data, timetag=timetag)
+    send_msg(client, '/tracker', data, timetag=timetag)
