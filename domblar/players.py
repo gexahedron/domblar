e6887e3549513bf6990024c8a8e5811d5adcb164
gexahedron <ulyanick@gmail.com>
2023-02-13 19:30:32 +0100

    added comments to unused functions

--- domblar/players.py
+++ domblar/players.py
@@ -4,42 +4,6 @@ from domblar.edo import triad, get_freq
 from domblar.sc3.client import SC3Client
 
 
-def play_voice(notes, timbre, scale, edo, client, dur=0.25, sus=None, delay=None):
-    if type(notes) is not list:
-        notes = [notes]
-    for note in notes:
-        freq = get_freq(note, scale, edo)
-        send_note_dur = dur
-        if sus:
-            send_note_dur = sus
-        timetag = time.time()
-        client.send_note(timbre, freq=freq, dur=send_note_dur, timetag=timetag)
-        time.sleep(dur)
-
-
-def play_voices(voices, timbres, scale, edo, client, dur=0.25, sus=None):
-    # check preconditions
-    for v in voices:
-        assert (len(v) == len(voices[0]))
-    assert (len(voices) == len(timbres))
-
-    for note_idx in range(len(voices[0])):
-        for v_idx, v in enumerate(voices):
-            notes = v[note_idx]
-            if not isinstance(notes, list):
-                notes = [notes]
-            for chord_note_idx, note in enumerate(notes):
-                freq = get_freq(note, scale, edo)
-                send_note_dur = dur
-                if sus:
-                    send_note_dur = sus
-                timetag = time.time()
-                client.send_note(
-                    timbres[v_idx],
-                    freq=freq, dur=send_note_dur * 2, timetag=timetag, channel=chord_note_idx)
-        time.sleep(dur)
-
-
 def play(chords, scale, edo, client: SC3Client,
          dur=0.25, sus=None, delay=None, synth_idx=[0], rep=1,
          muls=[], amps=[], voice_amps=[]):
@@ -83,6 +47,45 @@ def play(chords, scale, edo, client: SC3Client,
         time.sleep(sleep_dur)
 
 
+# TODO: currently unused
+def play_voice(notes, timbre, scale, edo, client, dur=0.25, sus=None, delay=None):
+    if type(notes) is not list:
+        notes = [notes]
+    for note in notes:
+        freq = get_freq(note, scale, edo)
+        send_note_dur = dur
+        if sus:
+            send_note_dur = sus
+        timetag = time.time()
+        client.send_note(timbre, freq=freq, dur=send_note_dur, timetag=timetag)
+        time.sleep(dur)
+
+
+# TODO: currently unused
+def play_voices(voices, timbres, scale, edo, client, dur=0.25, sus=None):
+    # check preconditions
+    for v in voices:
+        assert (len(v) == len(voices[0]))
+    assert (len(voices) == len(timbres))
+
+    for note_idx in range(len(voices[0])):
+        for v_idx, v in enumerate(voices):
+            notes = v[note_idx]
+            if not isinstance(notes, list):
+                notes = [notes]
+            for chord_note_idx, note in enumerate(notes):
+                freq = get_freq(note, scale, edo)
+                send_note_dur = dur
+                if sus:
+                    send_note_dur = sus
+                timetag = time.time()
+                client.send_note(
+                    timbres[v_idx],
+                    freq=freq, dur=send_note_dur * 2, timetag=timetag, channel=chord_note_idx)
+        time.sleep(dur)
+
+
+# TODO: currently unused
 def play_triads(sub_scale, degrees, dur, scale, edo, client):
     chords = []
     for deg in degrees:
