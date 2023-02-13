59609a89df718dab5b58681f8af50a676b80eb33
gexahedron <ulyanick@gmail.com>
2023-02-13 19:11:48 +0100

    Added analysis mode, where we don't use SuperCollider

--- domblar/domblar.py
+++ domblar/domblar.py
@@ -6,26 +6,35 @@ from domblar.players import play
 
 
 class Domblar:
-    def __init__(self, synth_count, context):
+    def __init__(self, synth_count, context, analysis_mode=False):
         self.client = SC3Client()
 
         assert context == 'dexed'
         self.vst_name = 'Dexed.vst3'
         self.init_preset_name = 'dexed_preset'
         self.tmp_preset_name = 'dexed_instrument'
+        self.analysis_mode = analysis_mode
 
-        self.synths = setup_instruments(
-            self.client,
-            synth_count, self.vst_name, self.init_preset_name)
+        if not self.analysis_mode:
+            self.synths = setup_instruments(
+                self.client,
+                synth_count, self.vst_name, self.init_preset_name)
 
     def set_synth(self, synth_idx, synth_name):
-        assign_instrument(synth_idx, synth_name, self.synths, self.client)
+        if not self.analysis_mode:
+            assign_instrument(synth_idx, synth_name, self.synths, self.client)
+
+    def set_param(self, synth_idx, param, val):
+        # assuming that val is float and in [0..1] range
+        self.client.set_param(synth_idx, param, int(val * 127))
 
     def play(self, chords, scale, edo,
              dur=0.25, sus=None, delay=None, synth_idx=[0], rep=1,
-             muls=[], amps=[]):
-        play(chords, scale, edo, self.client,
-             dur=dur, sus=sus, delay=delay, synth_idx=synth_idx, muls=muls, rep=rep, amps=amps)
+             muls=[], amps=[], voice_amps=[]):
+        if not self.analysis_mode:
+            play(chords, scale, edo, self.client,
+                dur=dur, sus=sus, delay=delay, synth_idx=synth_idx, muls=muls, rep=rep,
+                amps=amps, voice_amps=voice_amps)
 
     def stop_server(self):
         self.client.stop_server()
