7dd5ae44f7be62183b6c529deebd10abbec5c95b
gexahedron <ulyanick@gmail.com>
2023-02-13 19:13:18 +0100

    Added /set_param command, for setting synth params

--- domblar/sc3/client.py
+++ domblar/sc3/client.py
@@ -1,4 +1,7 @@
+import time
+
 import pyOSC3
+
 from domblar.music_math import freq_to_midi
 
 
@@ -7,20 +10,14 @@ class SC3Client:
         self.client = pyOSC3.OSCClient()
         self.client.connect((ip, port))
 
-    def send_msg(self, address, data=[], timetag=0):
-        """TODO: _summary_
-
-        Args:
-            client (_type_): _description_
-            address (_type_): _description_
-            data (_type_): _description_
-            timetag (int, optional): _description_. Defaults to 0.
-        """
+    def send_msg(self, address, data=[], timetag=0, sleep=None):
         bundle = pyOSC3.OSCBundle(time=timetag)
         msg = pyOSC3.OSCMessage(address=address)
         msg.append(data)
         bundle.append(msg)
         self.client.send(bundle)
+        if sleep:  # for some heavy operations
+            time.sleep(sleep)
 
     def send_note(self, synth_idx, freq=None, dur=0.25, amp=1.0, timetag=0, channel=0):
         """TODO: _summary_
@@ -56,5 +53,10 @@ class SC3Client:
     def load_preset(self, synth_idx, preset_name):
         self.send_msg('/load_preset', data=[synth_idx, preset_name])
 
+    def set_param(self, synth_idx, param, val):
+        self.send_msg('/set_param', data=[synth_idx, param, val])
+
     def print_params(self, synth_idx):
+        # TODO: rename param?
+        # i have clash in terminology (params here, and params in synth like amp or room)
         self.send_msg('/print_params', data=[synth_idx])
